cdd0421b3ba028f534e025296c2fef7e85788b85
James Gardiner <james@jgardiner.co.uk>
2017-07-25 09:29:24 +0100

    Correct typo. Change name

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ installRequirements = [
 ]
 
 setup(
-    name="sdc-rabbit-python",
+    name="sdc-rabbit",
     version=version,
     description="A shared library for SDC services that interact with RabbitMQ using Pika",
     author="J Gardiner",
     author_email="james@jgardiner.co.uk",
-    url="https://github.com/ONSdigital/sdxc-rabbit-python",
+    url="https://github.com/ONSdigital/sdc-rabbit",
     long_description=__doc__,
     classifiers=[
         "Operating System :: OS Independent",
